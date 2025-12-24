007ee745b6434bcaaa6a07840eb52b4e342b55f6
Venu5198 <152971243+Venu5198@users.noreply.github.com>
2025-12-24 23:31:06 +0530

    Delete app/tests directory

--- app/tests/test_health.py
+++
@@ -1,8 +0,0 @@
-from app.app import create_app
-
-def test_health_endpoint():
-    app = create_app()
-    client = app.test_client()
-
-    response = client.get("/health")
-    assert response.status_code == 200
